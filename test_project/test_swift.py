d9926a8501f6db4a108c68ead518c04bfcf6ed97
lamarwhitej <lamarwhitej@yahoo.com>
2016-11-02 20:10:07 -0500

    Update test_swift.py

--- test_project/test_swift.py
+++ test_project/test_swift.py
@@ -108,6 +108,7 @@ class ApiUptime(unittest.TestCase):
         conn.close()
 
     def test_create_delete_container(self, conn, service, times, container_name, object_name):
+	print("Running Swift during upgrades tests.")
 	output = []
         start_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
         down_time = None
