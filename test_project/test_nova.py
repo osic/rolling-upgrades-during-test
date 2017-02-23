d85733b95fcdc2ab5a551f47390be72685649366
lamarwhitej <lamarwhitej@yahoo.com>
2017-02-23 17:00:07 -0600

    Update test_nova.py

--- test_project/test_nova.py
+++ test_project/test_nova.py
@@ -111,13 +111,20 @@ class ApiUptime(unittest.TestCase):
 
     def _delete_server_list(self, nova_url, headers, name=None):
 	status = True
+	server_delete = ''
+
 	try:
             response = requests.get(nova_url + 'servers', headers=headers)
 	except Exception as e:
-	    print "Received: " + str(e) + " trying to delete server list"
-	    self.error_output += "Received: " + str(e) + " trying to delete server list"
+	    print "Received: " + str(e) + " trying to get list to delete servers"
+	    self.error_output += "Received: " + str(e) + " trying to get list to delete servers"
 	    status = False
 	    return status
+
+        if '401' in str(response):
+            print "Getting token, it may have expired."
+            self.headers = self.get_token()
+            return status
 
 	while any(c in str(response) for c in ('201','200','202')):
 
@@ -125,32 +132,25 @@ class ApiUptime(unittest.TestCase):
 
 	    for i in response['servers']:
 	        if i['name'] == name:
-	            server_delete = self.delete_server(nova_url, headers, i['id'])
-
-            if '401' in str(response):
-                print "Getting token, it may have expired."
-                self.headers = self.get_token()
-                return status
+		    while '204' not in str(server_delete):
+	                server_delete = self.delete_server(nova_url, headers, i['id'])
+			if '204' not in str(server_delete):
+				self.error_output += "Error trying to delete server: " + server_delete
             return status
 
 
     def create_server(self,url,headers,name, image, flavor, data):
 	avg_build_time = 0
 	url = url + '/servers'
-	response = requests.post(url, data=data,headers=headers)
+	try:
+	    response = requests.post(url, data=data,headers=headers)
+	except Exception as e:
+	    self.error_output = str(e) + " creating server on line 143"
 
 	if any(c in str(response) for c in ('201','202')):
             pass
-        elif any(c in str(response) for c in ('401','403','400','500')):
+        else:
             self.error_output = str(response) + " creating server on line 118"
-            return str(response), avg_build_time
-	else:
-            self.error_output = str(response) + " creating server on line 126"
-            try:
-                response = response.json()
-                self.server_id = response['server']['id']
-            except Exception as e:
-                pass
             return str(response), avg_build_time
 
 	#Wait until active
@@ -165,15 +165,17 @@ class ApiUptime(unittest.TestCase):
 	    pass
 	else:
 	    server_id = self.server_id
-
-	if server_id == None:
-	    return '204'
-	else:
-	    url = url + '/servers/' + str(server_id)
-	    response = str(requests.delete(url, headers=headers))
 	
-	    if '204' not in response:
-	        self.error_output = "Error deleting server: " + str(server_id) + " on line 146"
+	if server_id == None: return '204'
+
+        url = url + '/servers/' + str(server_id)
+        try:
+	    response = str(requests.delete(url, headers=headers))
+	except Exception as e:
+	    self.error_output = str(e) + " deleting server " + server_id + " on line 143"
+	
+	if '204' not in response:
+	    self.error_output = "Error deleting server: " + str(server_id) + " on line 146"
 	return response
 
     def report(self, conn, service, success, total, start_time, end_time, down_time, duration, avg_build_time):
@@ -268,52 +270,21 @@ class ApiUptime(unittest.TestCase):
             except Exception as e:
 	   	#print "Failed Nova: " + str(e)
 		status = 0
-		
+
 		if self.error_output != None:
 		    print self.error_output + ", " + str(e)
 		    self.error_output = self.error_output + ", " + str(e) + " line 245"
 		else:
 		    self.error_output = str(e) + " line 247"
-		
+
 		if server == None:
 		    pass
-		elif '401' in server or '401' in server_delete:
+		elif '401' in str(server) or '401' in str(server_delete):
 		    headers = False
-	        elif any(c in str(server) for c in ('403','500','503')):
-		    #If it is getting forbidden, there may be a limits issue
+	        else:
+		    #Remove all servers
 		    self._delete_server_list(nova_url, headers, name)
 		    sleep(1)
-                elif any(c in str(server) for c in ('ACTIVE','ERROR','BUILD')) or self.server_id:
-                    #Delete server
-		    server_delete = self.delete_server(nova_url, headers)
-
-		    #If server doesn't delete we need to try until it does
-		    while '204' not in server_delete:
-			#Gathering for logs
-			status_timestamp = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"))
-			done_time = time.time()
-                        total_down_time += (done_time - start_time)
-			duration += (done_time-start_time)
-			self.error_output += "-" + str(server_delete) + " trying to delete server on fail"
-
-                        #Write to status log
-                        self.write_status(service,status,status_timestamp,self.error_output,total_down_time,duration,str(build_start))
-                        output.append(False)
-
-                        if conn.poll() and conn.recv() == "STOP":
-                            break
-	                elif os.path.isfile('/usr/during.uptime.stop'):
-		            print "Ending Nova during testing."
-                            break
-
-                        if '401' in server_delete:
-                            print "Attempting to retrieve token and url"
-                            headers = self.get_token()
-                            nova_url = self.get_nova_url()
-
-		        print "Server delete failed.  Attempting to delete server"
-			sleep(5)
-                        server_delete = self.delete_server(nova_url, headers)
 
                 #Record down time
                 status_timestamp = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"))
